f654f90254e8161649677a9a83ef40ce3d5c0444
kendal125 <klopez80241@ufide.ac.cr>
2025-05-28 18:30:56 -0600

    Add files via upload

---
+++ clase2/src/clase2/Clase2.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lopez
+ */
+public class Clase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //algo
+        if (true){
+            JOptionPane.showMessageDialog(null, "el if dios siii");
+        }
+        int i=0;
+        if (i==0){
+            JOptionPane.showMessageDialog(null, "i es igual a 0");
+        }
+        if (i==1){
+            JOptionPane.showMessageDialog(null, "i es diferente a 0");
+        }
+        String variable1="hola1";
+        String variable2="hola1";
+        String variable3="hola2";
+        
+        if (variable1==variable2){
+            JOptionPane.showMessageDialog(null, "son iguales");
+        }
+        if (variable1=="hola1"){
+            JOptionPane.showMessageDialog(null, "son iguales");
+
+    }
+    
+}
